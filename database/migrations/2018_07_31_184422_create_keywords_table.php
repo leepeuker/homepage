a33c62dac80c80b4e8f52bbef72a68560821aba6
nativesys <lee.peuker@gmail.com>
2018-07-31 21:02:39 +0200

    Database changes and implementation of basic keyword for bookmarks classes/tables

--- database/migrations/2018_07_31_184422_create_keywords_table.php
+++ database/migrations/2018_07_31_184422_create_keywords_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBookmarksTable extends Migration
+class CreateKeywordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateBookmarksTable extends Migration
      */
     public function up()
     {
-        Schema::create('bookmarks', function (Blueprint $table) {
+        Schema::create('keywords', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('user_id');
-            $table->string('title');
-            $table->string('url')->unique();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('word');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateBookmarksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bookmarks');
+        Schema::dropIfExists('keywords');
     }
 }
---
+++ database/migrations/2018_07_31_185042_create_bookmarks_to_keywords_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookmarksToKeywordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmarks_to_keywords', function (Blueprint $table) {
+            $table->unsignedInteger('bookmark_id');
+            $table->unsignedInteger('keyword_id');
+            $table->string('word')->unique();
+            $table->foreign('bookmark_id')->references('id')->on('bookmarks')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('keyword_id')->references('id')->on('keywords')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookmarks_to_keywords');
+    }
+}
