573b0bf18bdae9858d2579d9d2d92f6c1b20ae26
nativesys <lee.peuker@gmail.com>
2018-07-31 12:31:58 +0200

    Changed failed login message

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'No user found matching these credentials.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
